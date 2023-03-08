954d5df8f4bcac2d3b7467cc1752ca43126ec2d8
Anh Tuấn Nguyễn <113050646+Anhtuan2710web@users.noreply.github.com>
2023-03-08 21:49:18 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Mẹ và các Chị';
+const giftUrl = 'https://www.facebook.com/tos.tuan.7';
+const eventName = 'Chúc Mừng ngày 8-03';
+const titleCard = 'Tặng người thân thương';
+const contentCard = 'Chúc Mẹ và các Chị của em 8/03 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với mọi người trong hôm nay và cả những ngày sau';
+
+const giftImage = 'gd1.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
